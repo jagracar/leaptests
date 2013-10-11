9d80cea3594a0d1c49f988a9e9d35ece592dbb00
jagracar <jagracar@gmail.com>
2013-10-11 15:13:56 +0200

    Improved the leap control
    Signed-off-by: jagracar <jagracar@gmail.com>

--- leaptests/src/ProcessingTest.java
+++ leaptests/src/ProcessingTest.java
@@ -1,69 +1,69 @@
 import processing.core.PApplet;
-
 import com.leapmotion.leap.CircleGesture;
 import com.leapmotion.leap.Controller;
 import com.leapmotion.leap.Gesture;
 import com.leapmotion.leap.GestureList;
 import com.leapmotion.leap.Hand;
+import com.leapmotion.leap.HandList;
 import com.leapmotion.leap.Vector;
-import com.leapmotion.leap.Gesture.State;
-
 import saito.objloader.OBJModel;
 
 public class ProcessingTest extends PApplet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private OBJModel model;
-	private OBJModel herschel;
-	private OBJModel itokawa;
-	private Controller leap;
-	private int controlHandId;
-	private int lightHandId;
-	private Vector controPos;
-	private Vector lightPos;
-	private int lastCircleEventId;
-	private int lastEventTime;
+    private OBJModel model;
+    private OBJModel herschel;
+    private OBJModel itokawa;
+    private Controller leap;
+    private Hand controlHand;
+    private Hand lightHand;
+    private int controlHandId;
+    private int lightHandId;
+    private Vector controlHandPos;
+    private Vector lightHandPos;
+    private int lastCircleEventId;
+    private int lastCircleEventTime;
 
-	public void setup() {
-		size(800, 800, P3D);
-		fill(255);
-		noStroke();
+    public void setup() {
+        size(800, 800, P3D);
+        fill(255);
+        noStroke();
 
-		// Load the models
-		herschel = new OBJModel(this, "HerschelExport.obj", POLYGON);
-		herschel.disableTexture();
-		herschel.scale(0.1f);
+        // Load the models
+        herschel = new OBJModel(this, "HerschelExport.obj", POLYGON);
+        herschel.disableTexture();
+        herschel.scale(0.1f);
 
-		itokawa = new OBJModel(this, "itokawa99846.obj", TRIANGLES);
-		itokawa.disableTexture();
-		itokawa.disableMaterial();
-		itokawa.scale(1000f);
+        itokawa = new OBJModel(this, "itokawa99846.obj", TRIANGLES);
+        itokawa.disableTexture();
+        itokawa.disableMaterial();
+        itokawa.scale(1000f);
 
-		// Start with the Herschel model
-		model = herschel;
+        // Start with the Herschel model
+        model = herschel;
 
-		// Leap setup
-		leap = new Controller();
-		leap.enableGesture(Gesture.Type.TYPE_CIRCLE);
-		leap.enableGesture(Gesture.Type.TYPE_SWIPE);
-		leap.enableGesture(Gesture.Type.TYPE_KEY_TAP);
-		leap.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
-		controlHandId = -1;
-		lightHandId = -1;
-		controPos = new Vector();
-		lightPos = new Vector();
-		lastEventTime = 0;
-		lastCircleEventId = -1;
+        // Leap motion setup
+        leap = new Controller();
+        leap.enableGesture(Gesture.Type.TYPE_CIRCLE);
+        leap.enableGesture(Gesture.Type.TYPE_SWIPE);
+        leap.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+        leap.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+        controlHand = leap.frame().hand(-1);
+        lightHand = leap.frame().hand(-1);
+        controlHandId = -1;
+        lightHandId = -1;
+        controlHandPos = new Vector();
+        lightHandPos = new Vector();
+        lastCircleEventId = -1;
+        lastCircleEventTime = 0;
 
-		if (leap.config().setFloat("Gesture.Circle.MinRadius", 10.0f)
-				&& leap.config().setFloat("Gesture.Circle.MinArc", TWO_PI)) {
-			leap.config().save();
-		}
-	}
+        if (leap.config().setFloat("Gesture.Circle.MinRadius", 10.0f) && leap.config().setFloat("Gesture.Circle.MinArc", TWO_PI)) {
+            leap.config().save();
+        }
+    }
 
-	public void draw() {
+    public void draw() {
         background(0);
-        println(frameRate + " fps");
 
         // Get the gestures
         GestureList gestures = leap.frame().gestures();
@@ -75,11 +75,13 @@ public class ProcessingTest extends PApplet {
             case TYPE_CIRCLE:
                 CircleGesture circle = new CircleGesture(gesture);
 
-                if (circle.isValid() && (millis() - lastEventTime) > 3000 && circle.id() != lastCircleEventId && circle.progress() > 1 && circle.radius() < 30) {
-                    println("Circle gesture detected!! " + circle.id() + " " + (millis() - lastEventTime) + " " + circle.radius());
-                    lastEventTime = millis();
+                if (circle.isValid() && circle.id() != lastCircleEventId && (millis() - lastCircleEventTime) > 3000
+                        && circle.progress() > 1 && circle.radius() < 30) {
+                    println("Circle gesture detected. ID: " + circle.id() + ", circle radius: " + circle.radius());
                     lastCircleEventId = circle.id();
+                    lastCircleEventTime = millis();
 
+                    // Change the model that will be shown
                     if (model.equals(herschel)) {
                         model = itokawa;
                     } else {
@@ -88,13 +90,13 @@ public class ProcessingTest extends PApplet {
                 }
                 break;
             case TYPE_SWIPE:
-                println("Swipe gesture detected!! " + frameCount);
+                println("Swipe gesture detected.");
                 break;
             case TYPE_KEY_TAP:
-                println("Key tap gesture detected!! " + frameCount);
+                println("Key tap gesture detected.");
                 break;
             case TYPE_SCREEN_TAP:
-                println("Screen tap gesture detected!! " + frameCount);
+                println("Screen tap gesture detected.");
                 break;
             default:
                 break;
@@ -102,51 +104,78 @@ public class ProcessingTest extends PApplet {
         }
 
         // Get the hand that controls the models if it's available
-        Hand controlHand = leap.frame().hand(controlHandId);
+        controlHand = leap.frame().hand(controlHandId);
 
-        if (controlHand.isValid()) {
-            controPos = controlHand.palmPosition();
-        } else {
-            controlHandId = leap.frame().hands().rightmost().id();
+        if (!controlHand.isValid()) {
+            // Use the right most hand if it's available
+            Hand rightHand = leap.frame().hands().rightmost();
+
+            if (rightHand.isValid()) {
+                controlHand = rightHand;
+                controlHandId = controlHand.id();
+            }
+        }
+
+        if (controlHand.isValid() && controlHand.palmPosition().isValid()) {
+            controlHandPos = controlHand.palmPosition();
         }
 
         // Get the hand that controls the illumination
-        Hand lightHand = leap.frame().hand(lightHandId);
+        if (lightHandId == controlHandId) {
+            lightHandId = -1;
+        }
 
-        if (lightHand.isValid() && lightHandId != controlHandId) {
-            lightPos = lightHand.fingers().frontmost().tipPosition();
-        } else {
-            // Loop over the hands and select the first that is available
-            for (int i = 0; i < leap.frame().hands().count(); i++) {
-                if (leap.frame().hands().get(i).id() != controlHandId) {
-                    lightHandId = leap.frame().hands().get(i).id();
-                    break;
+        lightHand = leap.frame().hand(lightHandId);
+
+        if (!lightHand.isValid()) {
+            // Use the left most hand if it's available
+            Hand leftHand = leap.frame().hands().leftmost();
+
+            if (leftHand.isValid() && leftHand.id() != controlHandId) {
+                lightHand = leftHand;
+                lightHandId = lightHand.id();
+            } else {
+                // Loop over the hands and select the first that is available
+                HandList hands = leap.frame().hands();
+
+                for (int i = 0; i < hands.count(); i++) {
+                    Hand hand = hands.get(i);
+
+                    if (hand.isValid() && hand.id() != controlHandId) {
+                        lightHand = hand;
+                        lightHandId = hand.id();
+                        break;
+                    }
                 }
             }
         }
 
+        if (lightHand.isValid() && lightHand.fingers().frontmost().tipPosition().isValid()) {
+            lightHandPos = lightHand.fingers().frontmost().tipPosition();
+        }
+
         // Set the illumination
-        float dirY = (-(lightPos.getY() - 150) / (float) 100) * 2f;
-        float dirX = ((lightPos.getX() + 50) / (float) 200) * 2f;
+        float dirY = -(lightHandPos.getY() - 150f) / 100f;
+        float dirX = (lightHandPos.getX() + 50f) / 100f;
         directionalLight(255, 255, 255, -dirX, -dirY, -1);
 
         // Draw the model
         pushMatrix();
-        translate(width / 2, height / 2);
-        rotateX(TWO_PI * (controPos.getY() - 150) / 150);
-        rotateY(-HALF_PI + TWO_PI * controPos.getX() / 300);
-        scale(200 / (200 + controPos.getZ()));
         pushStyle();
+        translate(width / 2f, height / 2f);
+        rotateX(TWO_PI * (controlHandPos.getY() - 150f) / 150f);
+        rotateY(-HALF_PI + TWO_PI * controlHandPos.getX() / 150f);
+        scale(200f / (200f + controlHandPos.getZ()));
         model.draw();
         popStyle();
         popMatrix();
     }
 
-	public void keyPressed() {
-		if (key == 'i') {
-			model = itokawa;
-		} else if (key == 'h') {
-			model = herschel;
-		}
-	}
+    public void keyPressed() {
+        if (key == 'i') {
+            model = itokawa;
+        } else if (key == 'h') {
+            model = herschel;
+        }
+    }
 }
